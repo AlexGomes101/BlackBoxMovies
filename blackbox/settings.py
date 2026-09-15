d155705cbe4716f1659af30a24dd1e69185812e3
Alex Gomes <alexgomes.augustinee@gmail.com>
2026-09-15 15:54:13 +0600

    Fix cloudinary_storage app order

--- blackbox/settings.py
+++ blackbox/settings.py
@@ -17,10 +17,10 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'cloudinary_storage',
+    'django.contrib.staticfiles',
     'cloudinary',
-    'movies',  # our app
+    'movies',
 ]
 
 MIDDLEWARE = [
